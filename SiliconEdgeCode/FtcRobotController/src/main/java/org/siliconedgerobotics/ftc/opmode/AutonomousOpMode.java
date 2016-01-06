77eb40a91767159469021471a22d77ca9c222532
Wei Gao <gaow@mycomets.org>
2016-01-06 16:54:53 -0500

    Create autonomous opmode file

---
+++ SiliconEdgeCode/FtcRobotController/src/main/java/org/siliconedgerobotics/ftc/opmode/AutonomousOpMode.java
@@ -0,0 +1,20 @@
+package org.siliconedgerobotics.ftc.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by Wei on 1/6/2016.
+ */
+
+// the fabled autonomous
+public class AutonomousOpMode extends OpMode {
+
+    public void init(){
+
+    }
+
+    public void loop(){
+        
+    }
+
+}
