66953416e18cdd1327e9f17bc827eabb165cc0a1
Wei Gao <gaow@mycomets.org>
2016-01-08 18:23:39 -0500

    Add common opmode class for shared methods

---
+++ SiliconEdgeCode/FtcRobotController/src/main/java/org/siliconedgerobotics/ftc/opmode/CommonOpMode.java
@@ -0,0 +1,14 @@
+package org.siliconedgerobotics.ftc.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by Wei on 1/8/2016.
+ */
+public abstract class CommonOpMode extends OpMode {
+
+    public abstract void init();
+    public abstract void loop();
+
+
+}
