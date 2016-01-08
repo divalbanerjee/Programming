51f216967da74879a993cc5844f94fe3c3312f50
Wei Gao <gaow@mycomets.org>
2016-01-08 17:50:38 -0500

    Remove reference to renamed opmode

--- SiliconEdgeCode/FtcRobotController/src/main/java/org/siliconedgerobotics/ftc/opmode/modules/AppendageHandler.java
+++ SiliconEdgeCode/FtcRobotController/src/main/java/org/siliconedgerobotics/ftc/opmode/modules/AppendageHandler.java
@@ -3,8 +3,6 @@ package org.siliconedgerobotics.ftc.opmode.modules;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.siliconedgerobotics.ftc.opmode.RefactoredOpMode;
-
 /**
  * Created by Wei on 1/6/2016.
  */
