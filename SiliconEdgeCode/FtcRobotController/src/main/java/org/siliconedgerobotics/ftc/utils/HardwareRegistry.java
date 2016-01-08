40e127092211abec85180e7402031304d31ac3c4
Wei Gao <gaow@mycomets.org>
2016-01-08 18:25:13 -0500

    Create class to register common hardware

---
+++ SiliconEdgeCode/FtcRobotController/src/main/java/org/siliconedgerobotics/ftc/utils/HardwareRegistry.java
@@ -0,0 +1,26 @@
+package org.siliconedgerobotics.ftc.utils;
+
+/**
+ * Created by Wei on 1/8/2016.
+ * Use this class to access common hardware on the robot.
+ */
+public class HardwareRegistry {
+
+    /*
+    * Names of hardware components in hardware map
+    */
+    // declare component names in hardware map
+    static final String LEFT_MOTOR_NAME = "lmotor";
+    static final String RIGHT_MOTOR_NAME = "rmotor";
+    static final String TAPE_MOTOR_NAME = "tapemotor";
+    static final String SLIDE_MOTOR_NAME = "slidemotor";
+    static final String BRUSH_MOTOR_NAME = "brushmotor";
+    static final String CONVEYOR_MOTOR_NAME = "conveyormotor";
+
+    // declare servo names in hardware map
+    static final String TAPE_ARM_SERVO_NAME = "tapeservo";
+    static final String BUCKET_SERVO_NAME = "bucketservo";
+
+
+
+}
